1f83ca300e533c0e8c6c026dc39bb0eb54ccab57
Atanas Atanasov <hackohackob@gmail.com>
2022-11-09 17:33:34 +0200

    added variables learning file

---
+++ Nasko/01.variables.js
@@ -0,0 +1,36 @@
+// const a = 5;
+// console.log(a);
+// a = 10;
+// console.log(a);
+// const varibales cannot be changed once declared
+
+
+// const name = "Gosho";
+// console.log(name);
+// console.log("Pesho")
+// we can create a const variable and log it's contents or we can log a random value
+
+
+// let firstName;
+// let secondName;
+// secondName = "Atanasov";
+// console.log(firstName);
+// difference between null and undefined
+
+
+
+// const stringVariable = "5";
+// const numberVariable = 2;
+// console.log(stringVariable, numberVariable);
+// console.log(stringVariable - numberVariable); // => "5" - 2 -> 5 - 2 -> 3
+
+// console.log("Petyr " + "Petrov");
+
+// const stringVariable = "5";
+// const numberVariable = 2;
+// console.log(stringVariable, numberVariable);
+// console.log(stringVariable + numberVariable); // => "5" + 2   -> "5" + "2" -> "52"
+
+// implicit data type conversion
+
+
