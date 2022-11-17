f498b7c164502b346e81ca6cdfb17f4363e50271
Atanas Atanasov <hackohackob@gmail.com>
2022-11-17 12:03:56 +0200

    array exercices

--- 05.Arrays/arraysHomework.js
+++ 05.Arrays/arraysHomework.js
@@ -1,23 +1,28 @@
-let numbers = ["22", "56", "884"];
+// let numbers = ["22", "56", "884"];
 
-let firstIndex = 0;
-let secondIndex = 1;
-let thirdIndex = 2;
+// let firstIndex = 0;
+// let secondIndex = 1;
+// let thirdIndex = 2;
 
-console.log (numbers[thirdIndex]);
+// console.log(numbers[thirdIndex]);
+let studentNames = ["Pesho", "Gosho", "Ivan"];
 
-// TEST CODE: ACCEPTED
+// // TEST CODE: ACCEPTED
 
-const numbers = [999, 545];
+// numbers = [999, 545];
 
-const arr2 = 0;
-const arr3 = 1;
+// const arr2 = 0;
+// const arr3 = 1;
 
-console . log(numbers [arr1  arr2]); join
+// console . log(numbers [arr1  arr2]); join
 
 //TEST CODE No1: ACCEPTED - bmw
 
+// let numbersAfterTen = [11, 12, 13];
 
+// console.log(numbersAfterTen)
+// console.log(numbersAfterTen[2])
+// console.log(numbersAfterTen[3])
 
 
 
@@ -47,66 +52,60 @@ console . log(numbers [arr1  arr2]); join
 
 
 
+// console.log(studentNames);
+// console.log(studentNames[0]);
+// console.log(studentNames.length);
+studentNames.push("Petko")
 
+studentNames.push('Nasko');
+studentNames.push('Nasko');
+studentNames.push('Nasko');
+studentNames.push('Nasko');
+studentNames.push('Nasko');
+studentNames.push('Nasko');
+studentNames.push('Nasko');
+studentNames.push('Nasko');
+studentNames.push('Vasko');
+// console.log(studentNames.length);
 
+// while(условие) {
+//     код
+// }
 
+// if (условие) {
+//     код
+// }
 
+// условие - true/false
 
 
+// let positiveNumber = 9;
+// console.log(positiveNumber);
 
+// positiveNumber = positiveNumber - 2;
+// console.log(positiveNumber);
 
+// let iterationNumber = 0;
 
 
+// while(positiveNumber > 0) {
+//     console.log('---');
+//     console.log('I ', iterationNumber);
 
+//     console.log('while start', positiveNumber)
+//     positiveNumber = positiveNumber - 1;
+//     console.log('while end', positiveNumber)
 
+//     iterationNumber = iterationNumber + 1;
+// }
 
+console.log(studentNames.length);
 
+for (let i = 0; i < studentNames.length; i++) {
+    console.log(i, '-', studentNames[i])
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// //i     i<10    action
+// 0       true    i++
+// 1       true    i++
+// 2       true    i++
