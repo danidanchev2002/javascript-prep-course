9d0326d755970fa55a69841d88cf417c6f68a921
danidanchev2002 <danidanchev2002@gmail.com>
2022-11-17 10:38:19 +0200

    arays WIP

---
+++ arraysHomework.js
@@ -0,0 +1,112 @@
+let numbers = ["22", "56", "884"];
+
+let firstIndex = 0;
+let secondIndex = 1;
+let thirdIndex = 2;
+
+console.log (numbers[thirdIndex]);
+
+// TEST CODE: ACCEPTED
+
+const numbers = [999, 545];
+
+const arr2 = 0;
+const arr3 = 1;
+
+console . log(numbers [arr1  arr2]); join
+
+//TEST CODE No1: ACCEPTED - bmw
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
