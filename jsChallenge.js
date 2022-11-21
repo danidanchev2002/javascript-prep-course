aa96b7eefb21bae1c6092487fcf556e2ff74ff9d
danidanchev2002 <danidanchev2002@gmail.com>
2022-11-22 01:26:13 +0200

    30DaysChallenge.js

---
+++ jsChallenge.js
@@ -0,0 +1,55 @@
+//let numOne = 3
+//let onTwo = 3
+
+//console.log(numOne == numTwo)
+
+
+
+//let space = ' '
+//let firstName = 'Yordan'
+//let lastName = 'Danchev'
+//let country = 'Bulgaria'      //Променливите са като домино ,а точката и запетаята са все едно да се акцентира кое ще се логва
+//let city = 'Sofia'            // ,затова като съм на две места точка и запетая не става.  
+//let language = 'JavaScript'
+//let job = 'developer'
+//let age = 20
+
+
+//let fullName = firstName + space + lastName
+//let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country;
+
+//console.log(personInfoOne)
+
+
+
+// Условие: "My name is Yordan Danchev i drive E61"
+
+//let space = ' '
+//let firstName = 'Yordan'
+//let secondName = 'Danchev'
+//let age = 20
+//let car = 'bmw E61'
+
+//let fullname = firstName + space + secondName
+//let danchevsInformation = '. My name' + '. is ' + fullname + '. Im ' + age + '. and I drive ' + car;
+
+//console.log(danchevsInformation)
+
+
+
+//let numbers = [5, 3, 9]
+//numbers [0] = 18   < така мога каквото и да пиша аз или user-a n или в случая 'numbers' го намира.
+
+//console.log(numbers)
+
+
+//let emptySpace = ' '
+//let googleSearch = ['bmw', 'mercedes', 'audi']
+//googleSearch[2] = 'Brabus'  //<това е търсачка (полето)
+//let modelBrabus = ['w11', 'w44', 'w55', 'w33']
+//modelBrabus[3] = 'AMG63'  //<това е търсачка (полето)
+
+//let carSearchResult = googleSearch[2] + emptySpace + modelBrabus[3];
+
+//console.log(carSearchResult)
+
